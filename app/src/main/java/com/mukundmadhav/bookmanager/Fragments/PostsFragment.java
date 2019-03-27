57ee89ece9143b7fdbe4ef3638b24bf32a195400
Mukund Madhav <monubwbs@gmail.com>
2019-03-27 19:48:04 +0530

    RecyclerView for Posts added

--- app/src/main/java/com/mukundmadhav/bookmanager/Fragments/PostsFragment.java
+++ app/src/main/java/com/mukundmadhav/bookmanager/Fragments/PostsFragment.java
@@ -8,15 +8,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mukundmadhav.bookmanager.Adapters.BookAdapter;
+import com.mukundmadhav.bookmanager.Models.Book;
 import com.mukundmadhav.bookmanager.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PostsFragment extends androidx.fragment.app.Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    List<Book> bookList;
+
     private String mParam1;
     private String mParam2;
+
+
+    RecyclerView bookRecyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    BookAdapter bookAdapter;
 
     private OnFragmentInteractionListener mListener;
 
@@ -46,7 +68,18 @@ public class PostsFragment extends androidx.fragment.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_posts, container, false);
+
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_posts, container, false);
+        bookRecyclerView = view.findViewById(R.id.posts_RView);
+        bookRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        bookRecyclerView.setHasFixedSize(true);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Posts");
+
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -67,6 +100,37 @@ public class PostsFragment extends androidx.fragment.app.Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                bookList = new ArrayList<>();
+
+                for(DataSnapshot bookSanp : dataSnapshot.getChildren()) {
+
+                    Book book = bookSanp.getValue(Book.class);
+                    bookList.add(book);
+
+                }
+
+                bookAdapter = new BookAdapter(getActivity(), bookList);
+                bookRecyclerView.setAdapter(bookAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
 
     public interface OnFragmentInteractionListener {
 
