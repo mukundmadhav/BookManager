3b96e8b6130d75991f1488e36deaad974f9c0cf3
Mukund Madhav <monubwbs@gmail.com>
2019-03-27 07:22:14 +0530

    popup layout added

--- app/src/main/java/com/mukundmadhav/bookmanager/Activities/HomeActivity.java
+++ app/src/main/java/com/mukundmadhav/bookmanager/Activities/HomeActivity.java
@@ -1,24 +1,45 @@
 package com.mukundmadhav.bookmanager.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.mukundmadhav.bookmanager.Fragments.BottomNavSheet;
+import com.mukundmadhav.bookmanager.Models.Book;
 import com.mukundmadhav.bookmanager.R;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,6 +48,12 @@ public class HomeActivity extends AppCompatActivity {
     Dialog popupAddPost;
     EditText popupName, popupPrice;
     ProgressBar popup_pbar;
+    Uri pickedImageUri = null;
+    FirebaseUser currentUser;
+    FirebaseAuth mAuth;
+
+    private static final int PReqCode = 2;
+    private static final int REQUESCODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +63,62 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.bottom_app_bar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab =  findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupAddPost.show();
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
         initPopup();
+        setupPopupImgClick();
 
 
+
+    }
+
+    private void setupPopupImgClick() {
+
+        popupChooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkForPermissions();
+            }
+        });
+
+    }
+
+    private void checkForPermissions() {
+
+
+        if(ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "Please accept for the required persmissions", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+
+        }
+
+        else {
+            openGallery();
+        }
+
+
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
     }
 
     private void initPopup() {
@@ -57,9 +137,97 @@ public class HomeActivity extends AppCompatActivity {
 
         //Click Listener for Done button
 
+        popupDoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+                popupDoneBtn.setVisibility(View.INVISIBLE);
+                popup_pbar.setVisibility(View.VISIBLE);
 
+                if(!popupName.getText().toString().isEmpty() && !popupPrice.getText().toString().isEmpty()
+                        && pickedImageUri!= null) {
+
+
+                    StorageReference storageBlogImgReg = FirebaseStorage.getInstance().getReference().child("blog_images");
+                    final StorageReference imgPath = storageBlogImgReg.child(pickedImageUri.getLastPathSegment());
+
+                    imgPath.putFile(pickedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            imgPath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    String imageDownloadLin = uri.toString();
+
+                                    Book book = new Book(popupName.getText().toString(),
+                                            popupPrice.getText().toString(),
+                                            imageDownloadLin,
+                                            currentUser.getUid(),
+                                            currentUser.getPhotoUrl().toString());
+
+                                    addPostToFirebase(book);
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+
+                                    showMessage(e.getMessage());
+                                    popupDoneBtn.setVisibility(View.VISIBLE);
+                                    popup_pbar.setVisibility(View.INVISIBLE);
+
+                                }
+                            });
+
+
+                        }
+                    });
+                }
+                else {
+                    showMessage("Please input in all the fields");
+                    popupDoneBtn.setVisibility(View.VISIBLE);
+                    popup_pbar.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+    }
+
+    private void addPostToFirebase(Book book) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Posts").push();
+
+        //Get key
+        String key = myRef.getKey();
+        book.setPostKey(key);
+
+        //now add to Firebase database
+        myRef.setValue(book).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                showMessage("Post Added Successfully");
+
+
+                popupDoneBtn.setVisibility(View.VISIBLE);
+                popup_pbar.setVisibility(View.INVISIBLE);
+
+                popupAddPost.dismiss();
+
+
+            }
+        });
+
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -77,7 +245,6 @@ public class HomeActivity extends AppCompatActivity {
 
         }
         else if(item.getItemId() == android.R.id.home) {
-            Log.i("com.mukundmadhav.bookmanager","marked Yes");
             BottomNavSheet bottomNavigationDrawerFrag = new BottomNavSheet();
             bottomNavigationDrawerFrag.show(getSupportFragmentManager(),bottomNavigationDrawerFrag.getTag());
         }
@@ -86,5 +253,12 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data !=null ) {
+            pickedImageUri = data.getData();
+            popupChooseImg.setImageURI(pickedImageUri);
+        }
+    }
 }
--- app/src/main/java/com/mukundmadhav/bookmanager/Models/Book.java
+++ app/src/main/java/com/mukundmadhav/bookmanager/Models/Book.java
@@ -1,4 +1,78 @@
 package com.mukundmadhav.bookmanager.Models;
 
+import com.google.firebase.database.ServerValue;
+
 public class Book {
+
+    private String title, price, picture, userId, userPic, postKey;
+    private Object timeStamp;
+
+
+    public Book(String title, String price, String picture, String userId, String userPic) {
+        this.title = title;
+        this.price = price;
+        this.picture = picture;
+        this.userId = userId;
+        this.userPic = userPic;
+        this.timeStamp = ServerValue.TIMESTAMP;;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPic() {
+        return userPic;
+    }
+
+    public void setUserPic(String userPic) {
+        this.userPic = userPic;
+    }
+
+    public String getPostKey() {
+        return postKey;
+    }
+
+    public void setPostKey(String postKey) {
+        this.postKey = postKey;
+    }
+
+    public Object getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Object timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
