edf2db4b38719b8e73bc4e56962c3f968f7df1b1
Mukund Madhav <monubwbs@gmail.com>
2019-04-10 23:15:55 +0530

    Book details activity

---
+++ app/src/main/java/com/mukundmadhav/bookmanager/activities/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.mukundmadhav.bookmanager.activities;
+
+import android.os.Bundle;
+
+import com.mukundmadhav.bookmanager.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
